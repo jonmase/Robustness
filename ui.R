b85d5d5d4d25a2c5d4897991ea95ef78961b9094
martinjhnhadley <martinjohnhadley@gmail.com>
2018-03-16 14:04:08 +0000

    Added links and switched to navbarPage

--- ui.R
+++ ui.R
@@ -3,48 +3,102 @@
 #		   Martin J. Hadley, Academic IT Research Support, University of Oxford
 # License: GPL v3, https://www.gnu.org/licenses/gpl.html
 
-fluidPage(title = "Robustness",
-          
-          #h2("Robustness"),
-          
-          sidebarLayout(
-            sidebarPanel(
-              width = 5,
-              uiOutput('resetable_input'),
-              fluidRow(align = "center",
-                       actionButton("rerun", "Rerun"),
-                       actionButton("reset", "Reset to Defaults")
-                       )
-            ),
-            mainPanel(width = 7,
-                      plotOutput("firstPlot", height = "600px"))
-            
+navbarPage(
+  "Robustness",
+  tabPanel(
+    "Visualisation",
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          width = 5,
+          uiOutput('resetable_input'),
+          fluidRow(align = "center",
+                   actionButton("rerun", "Rerun"),
+                   actionButton("reset", "Reset to Defaults")
+          )
+        ),
+        mainPanel(width = 7,
+                  plotOutput("firstPlot", height = "600px"))
+        
+      ),
+      p(
+        tagList(
+          "Created for the ",
+          a(
+            "MSc in Evidence-Based Health Care",
+            href = "https://www.conted.ox.ac.uk/about/msc-in-evidence-based-health-care",
+            target = "_blank"
           ),
-          p(
-            tagList(
-              "Created for the ",
-              a(
-                "MSc in Evidence-Based Health Care",
-                href = "https://www.conted.ox.ac.uk/about/msc-in-evidence-based-health-care",
-                target = "_blank"
-              ),
-              " at the ",
-              a(
-                "University of Oxford",
-                href = "https://www.ox.ac.uk",
-                target = "_blank"
-              ),
-              " by Tom Fanshawe and Jason Oke, with assistance from ",
-              a(
-                "MSD Learning Technologies",
-                href = "http://www.medsci.ox.ac.uk/msdlt",
-                target = "_blank"
-              ),
-              " and ",
-              a(
-                "Martin John Hadley",
-                href = "https://orcid.org/0000-0002-3039-6849",
-                target = "_blank"
-              )
-            )
-          ))
+          " at the ",
+          a(
+            "University of Oxford",
+            href = "https://www.ox.ac.uk",
+            target = "_blank"
+          ),
+          " by Tom Fanshawe and Jason Oke, with assistance from ",
+          a(
+            "MSD Learning Technologies",
+            href = "http://www.medsci.ox.ac.uk/msdlt",
+            target = "_blank"
+          ),
+          " and ",
+          a(
+            "Martin John Hadley",
+            href = "https://orcid.org/0000-0002-3039-6849",
+            target = "_blank"
+          )
+        )
+      )
+    )
+  ),
+  tabPanel(
+    "About",
+    fluidPage(
+      
+      p(
+        tagList(
+          "Created for the ",
+          a(
+            "MSc in Evidence-Based Health Care",
+            href = "https://www.conted.ox.ac.uk/about/msc-in-evidence-based-health-care",
+            target = "_blank"
+          ),
+          " at the ",
+          a(
+            "University of Oxford",
+            href = "https://www.ox.ac.uk",
+            target = "_blank"
+          ),
+          " by Tom Fanshawe and Jason Oke, with assistance from ",
+          a(
+            "MSD Learning Technologies",
+            href = "http://www.medsci.ox.ac.uk/msdlt",
+            target = "_blank"
+          ),
+          " and ",
+          a(
+            "Martin John Hadley",
+            href = "https://orcid.org/0000-0002-3039-6849",
+            target = "_blank"
+          )
+        )
+      ),
+      p(
+        "This Shiny app is hosted on the University of Oxford's ",
+        a(
+          "Interactive Data Network",
+          href = "http://idn.it.ox.ac.uk",
+          target = "_blank"
+        ),
+        " shinyapps.io subscription."
+      ),
+      p(strong("Author: "), "John Mason"),
+      p(strong("Code Deposit: "), a(
+        "https://doi.org/10.6084/m9.figshare.5966245",
+        href = "https://doi.org/10.6084/m9.figshare.5966245",
+        target = "_blank"
+      ))
+      
+    )
+  )
+)
